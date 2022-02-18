f193119f01e77a10caddc8955a946caae6fe36cd
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-02-18 10:29:32 +0100

    first commit

---
+++ examen/persona.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class persona {
+    
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int dni;
+    private String domicilio;
+    private ArrayList<String> personas = new ArrayList<String>();
+
+    public persona(String nombre,String apellido,int edad, int dni,String domicilio,ArrayList<String> personas){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.personas = personas;
+    }
+    public persona(){
+        nombre = "Maria";
+        edad = 25;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+    public void eliminarPersona(){
+
+    }
+}
